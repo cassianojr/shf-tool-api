aa317050402de4bf58ca3e2815b1685319c6d7c0
cassianojr <cassiano.jr.2009@hotmail.com>
2023-12-29 20:50:59 -0300

    feat: setup node-schedule

--- src/routes/delphi-round.routes.ts
+++ src/routes/delphi-round.routes.ts
@@ -1,4 +1,11 @@
 import express from 'express';
+import * as schedule from 'node-schedule';
+
+
+type MsgScheduled = {
+  to: string;
+  message: string;
+}
 
 const delphiRound = express.Router();
 delphiRound.post('/notify-start', (req, res) => {
@@ -6,7 +13,24 @@ delphiRound.post('/notify-start', (req, res) => {
 });
 
 delphiRound.post('/schedule-end', (req, res) => {
-    res.send('Hello, Delphi Round end!');
+
+  const { endAt, message, to } = req.body;
+  const dateEnd = new Date(endAt);
+
+  const msgScheduled = {
+    to,
+    message,
+  }
+
+  console.log("Delphi Round end at: ", dateEnd.toDateString(), dateEnd.toTimeString());
+
+  const job = schedule.scheduleJob(dateEnd, function (msg: MsgScheduled) {
+    console.log(`[${new Date().toTimeString()}] sent email to ${msg.to} with content: ${msg.message} `);
+  }.bind(null, msgScheduled));
+
+
+  res.status(200).send('ok');
 });
+
 
 export default delphiRound; 
