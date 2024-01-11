92c747f5029597311e5b0942d566469bf8b63a89
cassianojr <cassiano.jr.2009@hotmail.com>
2024-01-11 13:34:57 -0300

    CORS configuration

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, {Express} from 'express';
 import dotenv from 'dotenv';
 import delphiRound from './routes/delphi-round.routes';
+import cors from 'cors';
 
 dotenv.config();
 if(process.env.NODE_ENV === 'development') dotenv.config({ path: `.env.local`, override: true });
@@ -8,6 +9,7 @@ if(process.env.NODE_ENV === 'development') dotenv.config({ path: `.env.local`, o
 const app: Express = express();
 app.disable('x-powered-by');
 app.use(express.json());
+app.use(cors());
 
 app.use('/delphi-round', delphiRound);
 
